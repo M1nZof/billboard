963e27bb616f178152abc57de12d848812f449c1
M1nZof <maxykl38@gmail.com>
2023-07-09 16:06:14 +0300

    Added image of place preview

--- places/admin.py
+++ places/admin.py
@@ -1,16 +1,30 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from places.models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
     model = Place.imgs.through
+    extra = 0
+
+    fields = ['show_image', ]
+    readonly_fields = ['show_image']
+
+    def show_image(self, obj):
+        image_object = PlaceImage.objects.filter(pk=obj.placeimage_id).first()
+        return format_html(f'<img src="{image_object.image.url}" height="200">')
+    show_image.short_description = 'Фотографии'
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ('title', )
     inlines = [PlaceImageInline, ]
+    fieldsets = [
+        (None, {'fields': ['title', 'description_short', 'description_long', 'lng', 'lat']}),
+        ('Фотографии', {'fields': ['slug']})
+    ]
+    list_display = ('title', )
 
 
 @admin.register(PlaceImage)
--- places/models.py
+++ places/models.py
@@ -5,6 +5,9 @@ class PlaceImage(models.Model):
     description = models.CharField(max_length=200, verbose_name='Описание картинки')
     image = models.ImageField(verbose_name='Картинка')
 
+    def __str__(self):
+        return f'{self.pk} {self.description}'
+
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название локации')
