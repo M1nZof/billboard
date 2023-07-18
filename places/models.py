72a6e66b5dd43f5b5d9aa84822217eed8f7510b4
M1nZof <maxykl38@gmail.com>
2023-07-18 20:07:15 +0300

    Added ability to description fields to being blank

--- places/models.py
+++ places/models.py
@@ -13,8 +13,8 @@ class PlaceImage(models.Model):
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название локации', unique=True)
     imgs = models.ManyToManyField(PlaceImage, verbose_name='Картинки локации')
-    description_short = models.CharField(max_length=200, verbose_name='Короткое описание')
-    description_long = HTMLField()
+    description_short = models.TextField(null=True, blank=True, verbose_name='Короткое описание')
+    description_long = HTMLField(null=True, blank=True)
     lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
     slug = models.CharField(max_length=200, verbose_name='Латинизированное название локации')
