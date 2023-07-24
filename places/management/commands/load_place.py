9eb28f926d69b980580945ee247bf3ee50119fce
M1nZof <maxykl38@gmail.com>
2023-07-24 22:33:16 +0300

    Removed excess if-else block (raise_for_status used instead)

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -17,33 +17,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         response = requests.get(options['link_to_json'])
-        if response.reason == 'OK':
-            result = response.json()
-            title, description_short, description_long, coordinates = result['title'], result['description_short'], \
-                                                                      result['description_long'], result['coordinates']
-            lng, lat = coordinates['lng'], coordinates['lat']
-            imgs = result['imgs']
+        response.raise_for_status()
+        result = response.json()
+        title, description_short, description_long, coordinates = result['title'], result['description_short'], \
+                                                                  result['description_long'], result['coordinates']
+        lng, lat = coordinates['lng'], coordinates['lat']
+        imgs = result['imgs']
 
-            place, is_added = Place.objects.get_or_create(title=title, description_short=description_short,
-                                                          description_long=description_long, lng=lng, lat=lat)
+        place, is_added = Place.objects.get_or_create(title=title, description_short=description_short,
+                                                      description_long=description_long, lng=lng, lat=lat)
 
-            for i, img in enumerate(imgs, start=1):
-                response = requests.get(img)
-                if response.reason == 'OK':
-                    try:
-                        path_to_image = os.path.join(MEDIA_ROOT, (img.split('/')[-1]))
-                        with open(path_to_image, 'wb') as file:
-                            file.write(response.content)
-                        image_object = PlaceImage(image=path_to_image, place=place, order=i)
-                        image_object.save()
-                    except Exception as e:
-                        print(f'Error while adding photos to DB: {e}')
-                else:
-                    print(f'{title} | some error while downloading photos')
-                    break
-            if is_added:
-                print(f'{title} was successfully added!')
-            else:
-                print('Object already exist and was not rewrote!')
+        for i, img in enumerate(imgs, start=1):
+            response = requests.get(img)
+            response.raise_for_status()
+            try:
+                path_to_image = os.path.join(MEDIA_ROOT, (img.split('/')[-1]))
+                with open(path_to_image, 'wb') as file:
+                    file.write(response.content)
+                image_object = PlaceImage(image=path_to_image, place=place, order=i)
+                image_object.save()
+            except Exception as e:
+                print(f'Error while adding photos to DB: {e}')
+        if is_added:
+            print(f'{title} was successfully added!')
         else:
-            print(f'Something went wrong. {response.status_code}: {response.reason}')
+            print('Object already exist and was not rewrote!')
